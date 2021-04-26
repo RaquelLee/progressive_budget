4b7e319644ec94d55d0a02f1246e7d8711eef0b0
rsco <RaquelLee@users.noreply.github.com>
2021-04-26 12:45:17 -0400

    desploying pt3

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"))});
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
